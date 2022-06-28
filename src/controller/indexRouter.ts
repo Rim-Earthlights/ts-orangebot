74a6b8f195312b0eb4dbdebeb7b5f58d67c013ae
Rim Earthlights <iris.wind.code@gmail.com>
2022-06-28 23:30:29 +0900

    コメント追加

--- src/controller/indexRouter.ts
+++ src/controller/indexRouter.ts
@@ -5,9 +5,9 @@ import { DISCORD_CLIENT } from '../constant/constants';
 export const indexRouter = Express.Router();
 
 /**
- * / get
- * Index
- *
+ * / GET
+ * Index表示 特に今は指定していない.
+ * Botの管理UIでも作りたい所存.
  */
 indexRouter.get('/', (req: Express.Request, res: Express.Response) => {
     res.status(200).send({ result: true });
