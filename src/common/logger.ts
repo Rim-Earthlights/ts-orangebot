bbb423c1b7244355bbca376b7e3b4139dfc3ee38
Rim Earthlights <iris.wind.code@gmail.com>
2023-07-13 19:41:22 +0900

    logに保存する処理追加

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -1,7 +1,7 @@
 import dayjs from 'dayjs';
 import { Log } from '../model/models';
 import { DeepPartial } from 'typeorm';
-
+import { LogRepository } from '../model/repository/logRepository.js';
 /**
  * ログを出力する
  * @param gid
@@ -9,6 +9,8 @@ import { DeepPartial } from 'typeorm';
  * @param message
  */
 export async function put(log: DeepPartial<Log>) {
+    const repository = new LogRepository();
+    await repository.save(log);
     console.log(`[${dayjs().format('YYYY/MM/DD HH:mm:ss')}/${log.level}]: ${log.guild_id} | ${log.event}`);
     if (log.message) {
         console.log('> ' + log.message);
