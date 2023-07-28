e25086e28345d697c126cf25de90bae776cf29f4
Rim Earthlights <iris.wind.code@gmail.com>
2023-07-28 09:07:39 +0900

    loggerをクラスに修正

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -3,6 +3,26 @@ import { Log } from '../model/models';
 import { DeepPartial } from 'typeorm';
 import { LogRepository } from '../model/repository/logRepository.js';
 
+export class Logger {
+    static async put(log: DeepPartial<Log>) {
+        const repository = new LogRepository();
+        try {
+            await repository.save({
+                ...log,
+                message: log.message ? log.message.trimStart().replaceAll('\n', '/') : undefined
+            });
+            console.log(`[${dayjs().format('YYYY/MM/DD HH:mm:ss')}/${log.level}]: ${log.guild_id} | ${log.event}`);
+            if (log.message) {
+                console.log('> ' + log.message);
+            }
+            console.log('==================================================');
+        } catch (e) {
+            const err = e as Error;
+            console.error(err.message);
+        }
+    }
+}
+
 /**
  * ログを出力する
  * @param gid
