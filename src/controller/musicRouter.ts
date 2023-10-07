faf59932ee642d0a047502b5484a471f70a72ddb
Rim Earthlights <iris.wind.code@gmail.com>
2023-10-07 13:06:31 +0900

    router側のログをLoggerクラスに変更

--- src/controller/musicRouter.ts
+++ src/controller/musicRouter.ts
@@ -2,6 +2,8 @@ import Express from 'express';
 import { MusicRepository } from '../model/repository/musicRepository.js';
 import dayjs from 'dayjs';
 import { MusicInfoRepository } from '../model/repository/musicInfoRepository.js';
+import { Logger } from '../common/logger.js';
+import { LogLevel } from '../type/types.js';
 
 export const musicRouter = Express.Router();
 
@@ -12,12 +14,18 @@ export const musicRouter = Express.Router();
  * @param req.query.gid サーバID
  */
 musicRouter.get('/music', async (req: Express.Request, res: Express.Response) => {
-    console.log('===== /music:get =====');
-    console.log(`ip: ${req.ip}`);
-    console.log(`timestamp: ${dayjs().format('YYYY-MM-DD HH:mm:ss')}`);
-    console.log('======================');
     const gid = req.query.gid as string | undefined;
     const cid = req.query.cid as string | undefined;
+
+    Logger.put({
+        guild_id: gid,
+        channel_id: cid,
+        user_id: undefined,
+        level: LogLevel.INFO,
+        event: 'GET /music',
+        message: [`ip: ${req.ip}`]
+    });
+
     if (!gid || !cid) {
         res.status(400).send({ code: 400, message: 'gid/cid is required' });
         return;
